9f7da9234529fc2088596659c725d32b0e5296ff
javgarmon <javier.garces.m@gmail.com>
2022-11-15 17:18:29 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseMeBut();
+  });
+};
+
+let excuseMeBut = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let answer1 = who[Math.floor(Math.random() * who.length)];
+  let answer2 = what[Math.floor(Math.random() * what.length)];
+  let answer3 = when[Math.floor(Math.random() * when.length)];
+  return answer1 + " " + answer2 + " " + answer3;
 };
